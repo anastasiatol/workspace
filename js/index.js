ead657c257eac7543da963cad882b09ab0f9ada3
anastasiatol <tolstykoanastasia@gmail.com>
2017-08-13 23:36:54 +0300

    add modal windows (p1)

--- js/index.js
+++ js/index.js
@@ -22,9 +22,55 @@ function renderImageCollection(images) {
             h2.innerText = image.name + ' ' + image.surname;
             $(div).append(h2);
             $(".employees").append(div);
+            
+            // create modalwindow
+            var divModalBack = document.createElement('div');
+            var divModal = document.createElement('div');
+            $(divModalBack).addClass("modalWindow");
+            $(divModal).addClass("modalContent");
+            var photo = document.createElement("img");
+            photo.src = image.src;
+            $(divModal).append(photo);
+            var divInformation=document.createElement('div');
+            var p1 = document.createElement('p');
+            p1.innerText = image.name;
+            var p2 = document.createElement('p');
+            p2.innerText = image.surname;
+            var p3 = document.createElement('p');
+            p3.innerText = image.sex;
+            $(divInformation).append(p1);
+            $(divInformation).append(p2);
+            $(divInformation).append(p3);
+            $(divModal).append(divInformation);
+            var divRole = document.createElement('div');
+            divRole.innerHTML = image.role;
+            $(divModal).append(divRole);
+            $(divModalBack).append(divModal);
+            $(div).append(divModalBack);
+ //           var buttonClose = document.createElement('span');
+ //           buttonClose.innerHTML = 'close';
+ //           $(buttonClose).click (function (){
+ //               closeModalWindow(event);
+ //            });
+ //           $(divModal).append(buttonClose);
+                        
+            $(div).click (function (){
+                openModalWindow(event);
+            });
+            $(".employees").append(div);
         });
     }
 }
+
+function openModalWindow(ev){
+    var targetModal = ev.target;
+    targetModal.lastElementChild.style.display = "block";
+}
+//function closeModalWindow(ev){
+//    var targetModal = ev.target;
+//    targetModal.parentElement.style.display = "none";
+//}
+
 
 $(document).ready(function () {
     renderGallery();
