1ab93a8cebfa2e955638f7ae5a0441aa53781489
anastasiatol <tolstykoanastasia@gmail.com>
2017-08-15 00:01:40 +0300

    pro file modal window works

--- js/index.js
+++ js/index.js
@@ -1,59 +1,28 @@
-function getImagesFromServer() {
-    return $.get("js/images.json");
+function getEmployeeListFromServer() {
+    return $.get("js/employeeList.json");
 }
 
-function renderGallery() {
-        getImagesFromServer().then(function (data) {
-        renderImageCollection(data);
-        this.images = data;
+function employeeList() {
+        getEmployeeListFromServer().then(function (data) {
+        renderEmployeeList(data);
+        this.employeeList = data;
     }.bind(this))
 }
 
-function renderImageCollection(images) {
-     if (images) {
-        images.forEach(function (image) {
+function renderEmployeeList(employeeList) {
+     if (employeeList) {
+        employeeList.forEach(function (employee) {
             var img = document.createElement("img");
-            img.src = image.src;
+            img.src = employee.src;
             $(img).addClass("small");
             var div = document.createElement('div');
             $(div).addClass("whitebox");
             $(div).append(img);
+            $(div).attr('id',employee.id)
             var h2 = document.createElement('h2');
-            h2.innerText = image.name + ' ' + image.surname;
+            h2.innerText = employee.name + ' ' + employee.surname;
             $(div).append(h2);
-            $(".employees").append(div);
-            
-            // create modalwindow
-            var divModalBack = document.createElement('div');
-            var divModal = document.createElement('div');
-            $(divModalBack).addClass("modalWindow");
-            $(divModal).addClass("modalContent");
-            var photo = document.createElement("img");
-            photo.src = image.src;
-            $(divModal).append(photo);
-            var divInformation=document.createElement('div');
-            var p1 = document.createElement('p');
-            p1.innerText = image.name;
-            var p2 = document.createElement('p');
-            p2.innerText = image.surname;
-            var p3 = document.createElement('p');
-            p3.innerText = image.sex;
-            $(divInformation).append(p1);
-            $(divInformation).append(p2);
-            $(divInformation).append(p3);
-            $(divModal).append(divInformation);
-            var divRole = document.createElement('div');
-            divRole.innerHTML = image.role;
-            $(divModal).append(divRole);
-            $(divModalBack).append(divModal);
-            $(div).append(divModalBack);
- //           var buttonClose = document.createElement('span');
- //           buttonClose.innerHTML = 'close';
- //           $(buttonClose).click (function (){
- //               closeModalWindow(event);
- //            });
- //           $(divModal).append(buttonClose);
-                        
+            $(".employees").append(div);                       
             $(div).click (function (){
                 openModalWindow(event);
             });
@@ -63,8 +32,40 @@ function renderImageCollection(images) {
 }
 
 function openModalWindow(ev){
-    var targetModal = ev.target;
-    targetModal.lastElementChild.style.display = "block";
+    var idNumber = ev.target.id;
+    employeeList.forEach (function (employee) {
+        if (employee.id == idNumber) {
+            var divModalBack = document.createElement('div');
+            var divModal = document.createElement('div');
+            $(divModalBack).addClass("modalWindow");
+            $(divModal).addClass("modalContent");
+            var photo = document.createElement("img");
+            photo.src = employee.src;
+            $(divModal).append(photo);
+            var divInformation=document.createElement('div');
+            var p1 = document.createElement('p');
+            p1.innerText = employee.name;
+            var p2 = document.createElement('p');
+            p2.innerText = employee.surname;
+            var p3 = document.createElement('p');
+            p3.innerText = employee.sex;
+            $(divInformation).append(p1);
+            $(divInformation).append(p2);
+            $(divInformation).append(p3);
+            $(divModal).append(divInformation);
+            var divRole = document.createElement('div');
+            divRole.innerHTML = employee.role;
+            $(divModal).append(divRole);
+            $(divModalBack).append(divModal);
+            var buttonClose = document.createElement('span');
+            buttonClose.innerHTML = 'close';
+            $(buttonClose).click (function (){
+                $(".modalWindow").remove();
+            });
+            $(divModal).append(buttonClose);
+            $(".modal").append(divModalBack);
+        }
+    })
 }
 //function closeModalWindow(ev){
 //    var targetModal = ev.target;
@@ -73,5 +74,5 @@ function openModalWindow(ev){
 
 
 $(document).ready(function () {
-    renderGallery();
+    employeeList();
 });
